9ddf6dd98311a198f8938aa5fed9f95c3b2dc3d4
harramfatima <harramfatima35@gmail.com>
2024-05-20 10:44:05 +0500

    home

--- FindaCook/Views/HomePage.xaml.cs
+++ FindaCook/Views/HomePage.xaml.cs
@@ -5,18 +5,18 @@ namespace FindaCook.Views
 {
     public partial class HomePage : ContentPage
     {
-        private int currentIndex = 0;
-        private readonly int maxIndex = 1;
-        private readonly TimeSpan slideInterval = TimeSpan.FromSeconds(3);
-        private readonly System.Threading.Timer timer;
+       // private int currentIndex = 0;
+        // private readonly int maxIndex = 1;
+       // private readonly TimeSpan slideInterval = TimeSpan.FromSeconds(3);
+       // private readonly System.Threading.Timer timer;
         public HomePage()
         {
             InitializeComponent();
             BindingContext = new HomePageViewModel();
-            timer = new System.Threading.Timer(TimerCallback, null, slideInterval, slideInterval);
+            //timer = new System.Threading.Timer(TimerCallback, null, slideInterval, slideInterval);
         }
 
-        private void TimerCallback(object state)
+        /* private void TimerCallback(object state)
         {
 
             currentIndex = (currentIndex + 1) % (maxIndex + 1);
@@ -24,8 +24,8 @@ namespace FindaCook.Views
             {
                 imageSlider.Position = currentIndex;
             });
-        }
-
+        } 
+        */
     }
 }
 
